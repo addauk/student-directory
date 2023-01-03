02c8769447108f2ed9f57b7a9d76f8f76c432d0b
addauk <addauk@gmail.com>
2023-01-03 11:04:40 +0000

    Refactored directory to make use of method calls

--- directory.rb
+++ directory.rb
@@ -10,12 +10,24 @@ students = [
 "Freddy Krueger",
 "The Joker",
 "Joffrey Baratheon",
-"Norman Bates"]
-#and then print them
-puts "The students of Villains Academy"
-puts "_____________"
-students.each do |student|
-  puts student
+"Norman Bates"
+]
+def print_header
+  puts "The students of Villains Academy"
+  puts "_____________"
 end
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |student|
+    puts student
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call all the methods
+print_header()
+print(students)
+print_footer(students)
