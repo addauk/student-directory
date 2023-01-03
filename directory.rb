8d63d9fd8eb3a1f47fd7f7e24f95b76ea75fe663
addauk <addauk@gmail.com>
2023-01-03 15:52:26 +0000

    Added validation and symbolization to cohort

--- directory.rb
+++ directory.rb
@@ -2,14 +2,33 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  #create an emopty array
+  #create an empty array
   students = []
   #get the first name
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
+    puts "Please enter their cohort"
+    cohort = nil
+    until cohort
+      cohort = gets.chomp
+      cohort = validate_cohort(cohort)
+    end
+    puts "Please enter their country of birth"
+    cob = gets.chomp
+    puts "Please enter their date of birth"
+    dob = gets.chomp
+    puts "Please enter their favourite hobby"
+    hobby = gets.chomp
+    puts "Please enter their height in cm"
+    height = gets.chomp
     #add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name,
+      birth_country: cob,
+      hobby:hobby,
+      date_of_birth:dob,
+      height:height,
+      cohort: cohort}
     puts "Now we have #{students.count} students"
     #get another name from the user
     name = gets.chomp
@@ -18,9 +37,20 @@ def input_students
   students
 end
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "_____________"
+def validate_cohort(cohort)
+  valid =["january","february","march","april","may",
+  "june","july","august","september","october","november","december"]
+  cohort = cohort.downcase
+  if valid.include?(cohort)
+    return cohort.to_sym
+  end
+  puts "Please enter a valid cohort (a month)"
+  nil
+end
+
+def print_header()
+  puts "The students of Villains Academy".center(CENTER_SPACE)
+  puts "_____________".center(CENTER_SPACE)
 end
 
 def print(students)
@@ -28,20 +58,20 @@ def print(students)
   while s_index < students.count
     student = students[s_index]
     s_name = student[:name]
-    if s_name.chr == "A" && s_name.length <12
-      $stdout.print s_index + 1
-      puts ". #{s_name} (#{student[:cohort]} cohort)"
+    if s_name.chr == "A" && s_name.length <12 
+      puts "#{s_index + 1}. #{s_name} (#{student[:cohort]} cohort)".center(CENTER_SPACE)
     end
     s_index += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(CENTER_SPACE)
 end
 
 #nothing happens until we call all the methods
 students = input_students
+CENTER_SPACE = 50
 print_header
 print(students)
 print_footer(students)
