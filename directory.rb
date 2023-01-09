177915f43ffa925c8231be4243560f9df6a3d55c
addauk <addauk@gmail.com>
2023-01-09 10:16:40 +0000

    Refactored further into separate methods and made use of a more accesible variable for students

--- directory.rb
+++ directory.rb
@@ -1,9 +1,9 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  #create an empty array
-  students = []
   #get the first name
   name = gets.sub("\n","")
   #while the name is not empty, repeat this code
@@ -23,22 +23,20 @@ def input_students
     puts "Please enter their height in cm"
     height = gets.chomp
     #add the student hash to the array
-    students << {name: name,
+    @students << {name: name,
       birth_country: cob,
       hobby:hobby,
       date_of_birth:dob,
       height:height,
       cohort: cohort}
     plural = "student"
-    if students.count > 1 
+    if @students.count > 1 
       plural = "students"
     end
-    puts "Now we have #{students.count} #{plural}"
+    puts "Now we have #{@students.count} #{plural}"
     #get another name from the user
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
 
 def validate_cohort(cohort)
@@ -57,10 +55,25 @@ def print_header()
   puts "_____________".center(CENTER_SPACE)
 end
 
-def print(students)
+def print_footer
+  plural = "student"
+  if @students.count > 1 
+    plural = "students"
+  end
+  puts "Overall, we have #{@students.count} great #{plural}".center(CENTER_SPACE)
+end
+
+def print_menu
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def print_students_list
   s_index = 0
-  while s_index < students.count
-    student = students[s_index]
+  while s_index < @students.count
+    student = @students[s_index]
     s_name = student[:name]
     if s_name.chr == "A" && s_name.length <12 
       puts "#{s_index + 1}. #{s_name} (#{student[:cohort]} cohort)".center(CENTER_SPACE)
@@ -69,39 +82,34 @@ def print(students)
   end
 end
 
-def print_footer(students)
-  plural = "student"
-  if students.count > 1 
-    plural = "students"
-  end
-  puts "Overall, we have #{students.count} great #{plural}".center(CENTER_SPACE)
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
+
+def process(selection)
+  # 3. do what the user has asked
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit #this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
     # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit #this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    process(gets.chomp)
   end
 end
-
 #nothing happens until we call all the methods
 CENTER_SPACE = 50
 interactive_menu
